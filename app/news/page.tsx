fd104a7844782fced68386e9610c9cc49f435739
jimitas <nonajun805@gmail.com>
2024-07-27 09:49:03 +0900

    7-2-3完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,9 +1,15 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "../_components/Pagination";
 import { NEWS_LIST_LIMIT } from "../_constants";
 
 export default async function Page() {
-  const { contents: news } = await getNewsList({ limit: NEWS_LIST_LIMIT });
+  const { contents: news, totalCount } = await getNewsList({ limit: NEWS_LIST_LIMIT });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount}/>
+    </>
+  );
 }
